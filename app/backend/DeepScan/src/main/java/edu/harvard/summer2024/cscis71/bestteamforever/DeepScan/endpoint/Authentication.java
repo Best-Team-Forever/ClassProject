c7584cf9a22d0fa38caed1012ae71d165ed90435
Alejandro Chaparro <alc5808@g.harvard.edu>
2024-07-02 14:23:28 -0400

    Injected the UserRepository within the Authentication controller

--- app/backend/DeepScan/src/main/java/edu/harvard/summer2024/cscis71/bestteamforever/DeepScan/endpoint/Authentication.java
+++ app/backend/DeepScan/src/main/java/edu/harvard/summer2024/cscis71/bestteamforever/DeepScan/endpoint/Authentication.java
@@ -1,11 +1,14 @@
 package edu.harvard.summer2024.cscis71.bestteamforever.DeepScan.endpoint;
 
 import edu.harvard.summer2024.cscis71.bestteamforever.DeepScan.endpoint.response.AccessToken;
+import edu.harvard.summer2024.cscis71.bestteamforever.DeepScan.repository.UserRepository;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class Authentication {
+
+    private UserRepository userRepository;
 
     @PostMapping("/authenticate")
     public AccessToken authenticate(){
