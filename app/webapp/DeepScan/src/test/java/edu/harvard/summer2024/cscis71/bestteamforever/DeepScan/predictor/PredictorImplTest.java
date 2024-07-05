88d1c8735c217bb0e3f2659512475f181c45c62d
Alejandro Chaparro <alc5808@g.harvard.edu>
2024-07-05 00:53:22 -0400

    Added Predictor unit test

--- app/webapp/DeepScan/src/test/java/edu/harvard/summer2024/cscis71/bestteamforever/DeepScan/predictor/PredictorImplTest.java
+++ app/webapp/DeepScan/src/test/java/edu/harvard/summer2024/cscis71/bestteamforever/DeepScan/predictor/PredictorImplTest.java
@@ -3,30 +3,20 @@ package edu.harvard.summer2024.cscis71.bestteamforever.DeepScan.predictor;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 import org.mockito.Mockito;
+import org.springframework.web.client.RestClient;
 
 import java.io.ByteArrayInputStream;
 import java.io.IOException;
 
 public class PredictorImplTest {
-/*
+
     @Test
-    void whenPredictIsCalledProcessIsStarted() throws IOException {
+    void whenPredictIsCalledAIModelIsInvoked() throws IOException {
 
-        //setup
-        ProcessBuilder processBuilder = Mockito.mock(ProcessBuilder.class);
-        Process process = Mockito.mock(Process.class);
-        Mockito.when(processBuilder.start()).thenReturn(process);
-        Mockito.when(process.getInputStream()).thenReturn(new ByteArrayInputStream("0.67".getBytes()));
+        RestClient restClient = Mockito.mock(RestClient.class);
 
-        PredictorImpl predictor = new PredictorImpl(processBuilder);
-
-        //execute
-        predictor.predict();
-
-        //assert
-        Mockito.verify(processBuilder).start();
     }
-
+/*
     @Test
     void whenPredictionIsDoneTheResultIsReturned() throws IOException {
 
