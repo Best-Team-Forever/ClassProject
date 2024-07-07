ab574e1832c1c843619d93023b137e5ba7fd7440
Alejandro Chaparro <alc5808@g.harvard.edu>
2024-07-07 16:58:25 -0400

    Removing unnecessary files

--- app/webapp/DeepScan/src/main/java/edu/harvard/summer2024/cscis71/bestteamforever/DeepScan/predictor/Predictor.java
+++
@@ -1,8 +0,0 @@
-package edu.harvard.summer2024.cscis71.bestteamforever.DeepScan.predictor;
-
-import org.springframework.stereotype.Service;
-
-public interface Predictor {
-
-    double predict();
-}
